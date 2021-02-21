cfb48670e6c8ded39fc925c92ec7e29aacbf7f08
Malik <malikhezretm@gmail.com>
2021-02-21 22:42:05 +0800

    change url protocol from http to https to support data fetching on netlify production

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
-export const employeesUrl = `http://api.additivasia.io/api/v1/assignment/employees/`
+export const employeesUrl = `https://api.additivasia.io/api/v1/assignment/employees/`
 
 export const subordinatesUrl = (params?: string) => `${employeesUrl}${params}`
